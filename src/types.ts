44d1a1d8f437535ef5674a8cb941c07a20a47bf6
David Luna <luna.bistuer@gmail.com>
2019-06-20 08:01:54 +0200

    rework on typings

--- src/types.ts
+++ src/types.ts
@@ -7,27 +7,32 @@ interface AnyConstructor {
   new(...args: any[]): any;
 }
 
+// Ensures a type or a return type to be wrapped in a promise
+type Promisified<T> = T extends AnyFunction  ? (...args: Parameters<T>) => Promisified<ReturnType<T>> :
+                      T extends Promise<any> ? T : Promise<T>;
 
-type Promisified<Type> = Type extends Promise<any> ? Type : Promise<Type>;
 
-type PromisifiedFunction<F extends AnyFunction> = (...args: Parameters<F>) => Promisified<ReturnType<F>>;
 
-// Return type for processed functions
-type AtWorkFunctionReturn<T extends AnyFunction> = ReturnType<T> extends Promise<any> ? ReturnType<T> : Promise<ReturnType<T>>;
-// Type of the processed functions
-type AtWorkFunction<T extends AnyFunction> = (...args: Parameters<T>) => AtWorkFunctionReturn<T>;
-// Type of the processed properties
-type AtWorkProperty<T> = (val?: T) => Promise<T>;
-// Type of the param accepted by the function
-export type AtWorkParam = AnyConstructor | AnyFunction | Object;
+// // Return type for processed functions
+// type AtWorkFunctionReturn<T extends AnyFunction> = ReturnType<T> extends Promise<any> ? ReturnType<T> : Promise<ReturnType<T>>;
+// // Type of the processed functions
+// type AtWorkFunction<T extends AnyFunction> = (...args: Parameters<T>) => AtWorkFunctionReturn<T>;
+// // Type of the processed properties
+// type AtWorkProperty<T> = (val?: T) => Promise<T>;
+// // Type of the param accepted by the function
+// export type AtWorkParam = AnyConstructor | AnyFunction | Object;
 
-/**
- * type returned for objects or instances of classes
- * - properties accessible through promisified functions
- * - methods accesible and changed signature using promises to return
- */
+// /**
+//  * type returned for objects or instances of classes
+//  * - properties accessible through promisified functions
+//  * - methods accesible and changed signature using promises to return
+//  */
+// export type AtWorkInstance<T> = {
+//   [K in keyof T]: T[K] extends AnyFunction ? AtWorkFunction<T[K]> : AtWorkProperty<T[K]>;
+// }
+
 export type AtWorkInstance<T> = {
-  [K in keyof T]: T[K] extends AnyFunction ? AtWorkFunction<T[K]> : AtWorkProperty<T[K]>;
+  [K in keyof T]: T[K] extends AnyFunction ? Promisified<T[K]> : (val?: T[K]) => Promisified<T[K]>;
 }
 
 /**
@@ -38,5 +43,5 @@ export type AtWorkConstructor<T extends AnyConstructor> = {
 }
 
 export type AtWorkReturn<T> = T extends AnyConstructor ? AtWorkConstructor<T> :
-                              T extends AnyFunction ? AtWorkFunctionReturn<T> :
+                              T extends AnyFunction ? Promisified<T> :
                               AtWorkInstance<T>;
